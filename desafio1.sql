5bbdf25d189cc89959f5235cbbc20ff8aba0ea01
Aline <allinne.oliveira.ol@gmail.com>
2022-05-06 11:10:55 -0300

    correção desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -13,8 +13,8 @@ CREATE TABLE SpotifyClone.users(
     user_name VARCHAR(50) NOT NULL,
     user_age SMALLINT UNSIGNED NOT NULL,
     plan_id INT NOT NULL,
-    plan_date DATE AUTO_INCREMENT,
-    FOREIGN KEY (plan_id) REFERENCES SpotifyClone.plans(id_plan)
+    plan_date DATE NOT NULL,
+    FOREIGN KEY (plan_id) REFERENCES SpotifyClone.plans(plan_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.artists(
@@ -23,17 +23,9 @@ CREATE TABLE SpotifyClone.artists(
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.artist_followers(
-    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     artist_id INT NOT NULL,
-    FOREIGN KEY(artist_id) references artists(artist_id),
-    FOREIGN KEY(user_id) references users(user_id)
-) engine = InnoDB;
-
-CREATE TABLE SpotifyClone.artist_followers(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    artist_id INT NOT NULL,
+    PRIMARY KEY (user_id, artist_id),
     FOREIGN KEY(artist_id) references artists(artist_id),
     FOREIGN KEY(user_id) references users(user_id)
 ) engine = InnoDB;
@@ -41,27 +33,28 @@ CREATE TABLE SpotifyClone.artist_followers(
 CREATE TABLE SpotifyClone.albuns_list(
     album_id INT PRIMARY KEY AUTO_INCREMENT,
     artist_id INT NOT NULL,
-    release_year year AUTO_INCREMENT,
+    release_year YEAR,
     title VARCHAR(100) NOT NULL,
     FOREIGN KEY(artist_id) references artists(artist_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.songs_list(
    song_id INT PRIMARY KEY AUTO_INCREMENT,
-    artist_id INT NOT NULL,
     album_id INT NOT NULL,
     `name` VARCHAR(50) NOT NULL,
-    duration DATE AUTO_INCREMENT,
-    cost decimal NOT NULL,
-    FOREIGN KEY(artist_id) references users(user_id)
+    duration MEDIUMINT UNSIGNED NOT NULL,
+    FOREIGN KEY(album_id) references albuns_list(album_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.reproductions_history(
-    reproduction_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     song_id INT NOT NULL,
-    FOREIGN KEY(artist_id) references artists(artist_id),
-    FOREIGN KEY(song_id) references songs_list(song_id)
+    `date`DATETIME NOT NULL,
+    reproduction_id INT NULL,
+    PRIMARY KEY (user_id, song_id),
+    FOREIGN KEY(song_id) references songs_list(song_id),
+    FOREIGN KEY(user_id) references users(user_id)
+
 ) engine = InnoDB;
 
 
@@ -94,29 +87,29 @@ VALUES
   ('Tyler Isle'),
   ('Fog');
   
-INSERT INTO SpotifyClone.Followers (user_id, artist_id)
+INSERT INTO SpotifyClone.artist_followers(user_id, artist_id)
 VALUES
   (1, 1),
-  (1, 2),
+  (1, 4),
   (1, 3),
   (2, 1),
   (2, 3),
-  (3, 4),
+  (3, 2),
   (3, 1),
-  (4, 2),
+  (4, 4),
   (5, 5),
   (5, 6),
   (6, 6),
   (6, 3),
   (6, 1),
-  (7, 4),
+  (7, 2),
   (7, 5),
   (8, 1),
   (8, 5),
   (9, 6),
-  (9, 2),
+  (9, 4),
   (9, 3),
-  (10, 4),
+  (10, 2),
   (10, 6);
   
   INSERT INTO SpotifyClone.albuns_list (title, artist_id, release_year)
@@ -132,89 +125,89 @@ VALUES
   ('No guarantees', 5, 2015),
   ('Apparatus', 6, 2015);
   
-  INSERT INTO SpotifyClone.Songs (`name`, duration, album_id)
+  INSERT INTO SpotifyClone.songs_list(`name`, album_id, duration)
 VALUES
-  ('Honey', 79, 10),
-  ('Walking And Man', 229, 7),
-  ('Young And Father', 197, 6),
-  ('Diamond Power', 241, 4),
-  ("Let's Be Silly", 132, 4),
-  ('I Heard I Want To Be Alone', 120, 9),
-  ('Finding My Traditions', 179, 7),
-  ('Without My Love', 111, 6),
-  ('Baby', 136, 10),
-  ('Magic Circus', 105, 1),
-  ('Dance With Her Own', 116, 7),
-  ('Hard And Time', 135, 1),
-  ('Reflections Of Magic', 163, 9),
-  ('I Ride Alone', 151, 7),
-  ("Honey, I'm A Lone Wolf", 150, 3),
-  ('Honey, So Do I', 207, 4),
-  ('Rock His Everything', 223, 10),
-  ('Soul For Us', 200, 8),
-  ("Wouldn't It Be Nice", 213, 8),
-  ("He Heard You're Bad For Me", 154, 6),
-  ("He Hopes We Can't Stay", 210, 2),
-  ('Walking And Game', 123, 4),
-  ('Time Fireworks', 152, 10),
-  ('Troubles Of My Inner Fire', 203, 9),
-  ('Celebration Of More', 146, 9),
-  ('You Make Me Feel So..', 83, 5),
-  ("He's Walking Away", 159, 3),
-  ("He's Trouble", 138, 3),
-  ('Thang Of Thunder', 240, 6),
-  ('Words Of Her Life', 185, 4),
-  ("Sweetie, Let's Go Wild", 139, 5),
-  ('She Knows', 244, 6),
-  ('History Of My Roses', 222, 8),
-  ('Home Forever', 231, 8),
-  ('Fantasy For Me', 100, 10),
-  ('Without My Streets', 176, 3),
-  ('Need Of The Evening', 190, 1),
-  ('I Know I Know', 117, 2),
-  ("She Thinks I Won't Stay Tonight", 166, 5),
-  ('You Cheated On Me', 95, 4);
+  ('Soul For Us', 1, 200),
+  ('Reflections Of Magic', 1, 163),
+  ('Dance With Her Own', 1, 116),
+  ('Troubles Of My Inner Fire', 2, 203),
+  ('Time Fireworks', 2, 152),
+  ('Magic Circus', 3, 105),
+  ('Honey, So Do I', 3, 207),
+  ('Sweetie, Let\'s Go Wild', 3, 139),
+  ('She Knows', 3, 244),
+  ('Fantasy For Me', 4, 100),
+  ('Celebration Of More', 4, 146),
+  ('Rock His Everything', 4, 223),
+  ('Home Forever', 4, 231),
+  ('Diamond Power', 4, 241),
+  ('Let\'s Be Silly', 4, 132),
+  ('Thang Of Thunder', 5, 240),
+  ('Words Of Her Life', 5, 185),
+  ('Without My Streets', 5, 176),
+  ('Need Of The Evening', 6, 190),
+  ('History Of My Roses', 6, 222),
+  ('Without My Love', 6, 111),
+  ('Walking And Game', 6, 123),
+  ('Young And Father', 6, 197),
+  ('Finding My Traditions', 7, 179),
+  ('Walking And Man', 7, 229),
+  ('Hard And Time', 7, 135),
+  ('Honey, I\'m A Lone Wolf', 7, 150),
+  ('She Thinks I Won\'t Stay Tonight', 8, 166),
+  ('He Heard You\'re Bad For Me', 8, 154),
+  ('He Hopes We Can\'t Stay', 8, 210),
+  ('I Know I Know', 8, 117),
+  ('He\'s Walking Away', 9, 159),
+  ('He\'s Trouble', 9, 138),
+  ('I Heard I Want To Bo Alone', 9, 120),
+  ('I Ride Alone', 9, 151),
+  ('Honey', 10, 79),
+  ('You Cheated On Me', 10, 95),
+  ('Wouldn\'t It Be Nice', 10, 213),
+  ('Baby', 10, 136),
+  ('You Make Me Feel So..', 10, 83);
   
-  INSERT INTO SpotifyClone.reproductions_history (user_id, song_id, `date`)
+  INSERT INTO SpotifyClone.reproductions_history(user_id, song_id, `date`)
 VALUES
-  (1, 1, '2020-02-28 10:45:55'),
-  (1, 2, '2020-05-02 05:30:35'),
-  (1, 3, '2020-03-06 11:22:33'),
-  (1, 4, '2020-08-05 08:05:17'),
-  (1, 5, '2020-09-14 16:32:22'),
-  (2, 6, '2020-01-02 07:40:33'),
-  (2, 7, '2020-05-16 06:16:22'),
-  (2, 8, '2020-10-09 12:27:48'),
-  (2, 9, '2020-09-21 13:14:46'),
-  (3, 10, '2020-11-13 16:55:13'),
-  (3, 11, '2020-12-05 18:38:30'),
-  (3, 12, '2020-07-30 10:00:00'),
-  (4, 13, '2021-08-15 17:10:10'),
-  (4, 14, '2021-07-10 15:20:30'),
-  (4, 15, '2021-01-09 01:44:33'),
-  (5, 16, '2020-07-03 19:33:28'),
-  (5, 17, '2017-02-24 21:14:22'),
-  (5, 4, '2020-08-06 15:23:43'),
-  (5, 18, '2020-11-10 13:52:27'),
-  (6, 19, '2019-02-07 20:33:48'),
-  (6, 20, '2017-01-24 00:31:17'),
-  (6, 21, '2017-10-12 12:35:20'),
+  (1, 36, '2020-02-28 10:45:55'),
+  (1, 25, '2020-05-02 05:30:35'),
+  (1, 23, '2020-03-06 11:22:33'),
+  (1, 14, '2020-08-05 08:05:17'),
+  (1, 15, '2020-09-14 16:32:22'),
+  (2, 34, '2020-01-02 07:40:33'),
+  (2, 24, '2020-05-16 06:16:22'),
+  (2, 39, '2020-09-21 13:14:46'),
+  (2, 21, '2020-10-09 12:27:48'),
+  (3, 6, '2020-11-13 16:55:13'),
+  (3, 3, '2020-12-05 18:38:30'),
+  (3, 26, '2020-07-30 10:00:00'),
+  (4, 2, '2021-08-15 17:10:10'),
+  (4, 35, '2021-07-10 15:20:30'),
+  (4, 27, '2021-01-09 01:44:33'),
+  (5, 7, '2020-07-03 19:33:28'),
+  (5, 12, '2017-02-24 21:14:22'),
+  (5, 14, '2020-08-06 15:23:43'),
+  (5, 1, '2020-11-10 13:52:27'),
+  (6, 38, '2019-02-07 20:33:48'),
+  (6, 29, '2017-01-24 00:31:17'),
+  (6, 30, '2017-10-12 12:35:20'),
   (6, 22, '2018-05-29 14:56:41'),
-  (7, 23, '2018-05-09 22:30:49'),
-  (7, 24, '2020-07-27 12:52:58'),
-  (7, 25, '2018-01-16 18:40:43'),
-  (8, 9, '2018-03-21 16:56:40'),
-  (8, 26, '2020-10-18 13:38:05'),
-  (8, 27, '2019-05-25 08:14:03'),
-  (8, 28, '2021-08-15 21:37:09'),
-  (9, 29, '2021-05-24 17:23:45'),
-  (9, 30, '2018-12-07 22:48:52'),
-  (9, 31, '2021-03-14 06:14:26'),
-  (9, 32, '2020-04-01 03:36:00'),
-  (10, 33, '2017-02-06 08:21:34'),
-  (10, 8, '2017-12-04 05:33:43'),
-  (10, 17, '2017-07-27 05:24:49'),
-  (10, 34, '2017-12-25 01:03:57');
+  (7, 5, '2018-05-09 22:30:49'),
+  (7, 4, '2020-07-27 12:52:58'),
+  (7, 11, '2018-01-16 18:40:43'),
+  (8, 39, '2018-03-21 16:56:40'),
+  (8, 40, '2020-10-18 13:38:05'),
+  (8, 32, '2019-05-25 08:14:03'),
+  (8, 33, '2021-08-15 21:37:09'),
+  (9, 16, '2021-05-24 17:23:45'),
+  (9, 8, '2021-03-14 06:14:26'),
+  (9, 17, '2018-12-07 22:48:52'),
+  (9, 9, '2020-04-01 03:36:00'),
+  (10, 20, '2017-02-06 08:21:34'),
+  (10, 21, '2017-12-04 05:33:43'),
+  (10, 12, '2017-07-27 05:24:49'),
+  (10, 13, '2017-12-25 01:03:57');
   
   
 
