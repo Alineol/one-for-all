a8753bf9cc820028abfd406775cc3b7c428f4ef1
Aline <allinne.oliveira.ol@gmail.com>
2022-05-06 12:17:06 -0300

    desafio3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+SELECT u.user_name as usuario, count(r.user_id) AS qtde_musicas_ouvidas,
+TRUNCATE((SUM(s.duration))/60,2) AS total_minutos
+FROM SpotifyClone.users AS u
+INNER JOIN SpotifyClone.reproductions_history As r
+ON u.user_id = r.user_id
+INNER JOIN SpotifyClone.songs_list As s
+ON r.song_id = s.song_id
+GROUP BY user_name
+ORDER BY user_name;
